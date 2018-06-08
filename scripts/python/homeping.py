ae66667f56a2f0af1d66a5b85965b295bb0799d0
scoday <me@scoday.tokyo>
2018-06-08 11:57:12 +0900

    Homeping changes and things and stuff.

--- scripts/python/homeping.py
+++ scripts/python/homeping.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # Good example of if / else and os use
 import os
-hostname = "gw01" #example
-response = os.system("ping -c 1 " + hostname)
+# hostname = "gw01" #example
+hostname = "pi.beastieco.com"
+# response = os.system("ping -c 1 " + hostname)
+response = os.system("ping " + hostname)
 
 if response == 0:
   print hostname, 'is up!'
