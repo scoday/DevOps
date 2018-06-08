24f413bb261d5039c20d1a9c3fd4d608c9188912
scoday <me@scoday.tokyo>
2018-06-08 12:20:58 +0900

    Latest changes just adding a function for some logging potential.

--- scripts/python/MIT_6.S095/1.py
+++ scripts/python/MIT_6.S095/1.py
@@ -6,6 +6,14 @@
 caps = ['F', 'F', 'B', 'B', 'B', 'F', 'B', 'B', 'B', 'F', 'F', 'B', 'F']
 caps2 = ['F', 'F', 'B', 'B', 'B', 'F', 'B', 'B', 'B', 'F', 'F', 'F', 'F']
 
+# if verbose:
+#    def verbosePrint(*args):
+#        for arg in args:
+#           print arg,
+#        print
+# else:
+#    verbosePrint = lambda *a: None # Nothing function
+
 def pleaseConform(caps):
     # Initialize
     start = 0
@@ -17,10 +25,33 @@ def pleaseConform(caps):
     for i in range(1, len(caps)):
         if caps [start] != caps[i]:
             # each interval "is" a tuple with "3" elements (start, end, type)
-        intervals.append((start, i-1, caps[start]))
+            intervals.append((start, i-1, caps[start]))
 
-        if caps [start] == 'F':
-            forward += 1
-        else:
-            backward += 1
+            if caps [start] == 'F':
+                forward += 1
+            else:
+                backward += 1
             start = 1
+
+    # Add the last interval to complete the execution
+    intervals.append((start, len(caps) - 1, caps[start]))
+    if caps[start] == 'F':
+        forward += 1
+    else:
+        backward += 1
+
+## print (intervals)
+## print (forward, backward)
+    if forward < backward:
+        flip = 'F'
+    else:
+        flip = 'B'
+    for t in intervals:
+        if t[2] == flip:
+            # if t[0] == t[1] change the output
+            print ('People in positions', t[0],
+                   'through', t[1], 'flip your caps!')
+
+# verbosePrint
+pleaseConform(caps)
+# pleaseConform(caps2)
