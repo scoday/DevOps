6b08719d44b5ab0d3a6927f4c8ce110dd2d22427
scoday@gw01 <scoday@gw01.scoday.local>
2018-07-13 08:00:15 +0900

    Changing directory structure to reflect non SLES configs.

---
+++ scripts/python/network/multicheck.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# Good example of if / else and os use
+import os
+# hostname = "gw01" #example
+hostname = "pi.beastieco.com"
+response = os.system("ping -c 1 " + hostname)
+# response = os.system("ping " + hostname)
+
+if response == 0:
+    print hostname, 'is up!'
+else:
+    print hostname, 'is down!'
