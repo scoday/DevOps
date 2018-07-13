6973500a032fbd851a8eeab405e8203a07da4df4
scoday@gw01 <scoday@gw01.scoday.local>
2018-07-14 02:00:03 +0900

    Fixing named.

--- dockerfiles/suse/bind9/files/start.sh
+++ dockerfiles/suse/bind9/files/start.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 #
-/usr/sbin/named -c /var/lib/named/etc/named.conf.local 
+
+start_named_foreground() {
+    /usr/sbin/named -f -c /var/lib/named/etc/named.conf.local 
+}
+
+start_named_foreground
