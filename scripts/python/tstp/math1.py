d8aea2826d0b9445b439d8fe8fd2634ea1c2c86e
scoday@heimat01 <scoday@heimat01.scoday.local>
2018-07-16 13:42:02 +0900

    Fixing up things and stuffs.

---
+++ scripts/python/tstp/math1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#
+# Intended to just highlight some math things and a
+# range 
+
+import math
+
+# Initialize variables
+l = 0
+w = 0
+d = math.sqrt(1**2 + w**2)
+
+def diagonalLen(d):
+    l = 4
+    w = 10
+    print("The value is: ",d, "Questions?")
+
+diagonalLen(d)
