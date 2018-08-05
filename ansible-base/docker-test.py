b00cd46468c6e0bf51953a8c4e12435a6dd6637c
scoday <scoday@scosuse01.ascent.jp>
2018-08-05 12:39:54 +0900

    Redoing the ansible pieces again for latest updates.

--- ansible-base/docker-test.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-# Modify this to use suse / ubuntu / etc.
-docker run -d -p 2222:22 --name ansible_test \
-    -v ~/.ssh/id_rsa.pub:/home/ubuntu/.ssh/authorized_keys \
-    philm/ansible_target:ubuntu1404
